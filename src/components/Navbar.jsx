af7b511e840bdf79ef96f5cb89e592f986f31cda
edwin141999 <183433@ids.upchiapas.edu.mx>
2023-01-11 00:28:08 -0600

    front navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React from "react";
+import Logo from "../images/pokemon-logo.png";
+import "./Navbar.css";
 
 export default function Navbar() {
-  return (
-    <div>Navbar</div>
-  )
+    return (
+        <div className="navbar">
+            <img src={Logo} alt="" className="navbar-img" />
+            <input
+                type="text"
+                placeholder="Enter Pokemon name or id..."
+                className="navbar-search"
+            />
+            <div className="navbar-div-filter">
+                <h4>Filter by Type</h4>
+                <select name="type" id="type" className="navbar-filter-type" />
+            </div>
+            <div className="navbar-div-filter">
+                <h4>Filter by Abilities</h4>
+                <select
+                    name="abilities"
+                    id="abilities"
+                    className="navbar-filter-abilities"
+                />
+            </div>
+        </div>
+    );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,9 +3,9 @@ import Card from "../components/Card";
 import "./Home.css";
 
 export default function Home() {
-    const [pokemon, setPokemon] = useState([]); 
+    const [pokemon, setPokemon] = useState([]);
     const getPokemon = () => {
-        fetch("https://pokeapi.co/api/v2/pokemon?offset=0&limit=50")
+        fetch("https://pokeapi.co/api/v2/pokemon?offset=0&limit=56")
             .then((response) => response.json())
             .then((data) => {
                 setPokemon(data.results);
@@ -18,7 +18,13 @@ export default function Home() {
 
     return (
         <div className="home-div">
-            {pokemon.map((pokemon) => <Card key={pokemon.name} name={pokemon.name} url={pokemon.url} />)}
+            {pokemon.map((pokemon) => (
+                <Card
+                    key={pokemon.name}
+                    name={pokemon.name}
+                    url={pokemon.url}
+                />
+            ))}
         </div>
     );
 }
